39108fe1b67ef94c0bd8d9161a801bd3ccca2d9b
Uenes Ferreira <ueneslucio@gmail.com>
2018-08-19 21:16:39 -0300

    Correction of scope bug

--- src/main/java/com/monsanto/interview/FortuneCookieGenerator/FortuneCookieController.java
+++ src/main/java/com/monsanto/interview/FortuneCookieGenerator/FortuneCookieController.java
@@ -1,12 +1,14 @@
 package com.monsanto.interview.FortuneCookieGenerator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Scope("request") // By default, controllers in Spring are singletons. 
 public class FortuneCookieController {
 
     @Autowired
